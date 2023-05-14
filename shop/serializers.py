691a8ab7e5f936220472a0cab8ef7c547c9e2cac
Emil-Ka <emilkaraev55@gmail.com>
2023-05-14 15:26:48 +0300

    added shoes api

--- shop/serializers.py
+++ shop/serializers.py
@@ -6,35 +6,3 @@ class ShoesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shoes
         fields = '__all__'
-
-
-class UpdateShoesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Shoes
-        fields = '__all__'
-        extra_kwargs = {
-            'name': {
-                'required': False
-            },
-            'image': {
-                'required': False
-            },
-        }
-
-    def validate(self, attrs):
-        if self.initial_data.get('id') is None:
-            raise serializers.ValidationError({'error': 'id is required'})
-
-        return self.initial_data
-
-
-class DeleteShoesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Shoes
-        fields = ['id']
-
-    def validate(self, attrs):
-        if self.initial_data.get('id') is None:
-            raise serializers.ValidationError({'error': 'id is required'})
-
-        return self.initial_data
--- shop/views.py
+++ shop/views.py
@@ -1,71 +1,9 @@
-from django.forms import model_to_dict
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.exceptions import NotFound
-from rest_framework.decorators import action
 
 from shop.models import Shoes
-from shop.serializers import ShoesSerializer, UpdateShoesSerializer, DeleteShoesSerializer
+from shop.serializers import ShoesSerializer
 
 
 class ShoesViewSet(ModelViewSet):
     queryset = Shoes.objects.all()
     serializer_class = ShoesSerializer
-
-    def get_serializer_class(self):
-        if self.action == 'update_shoes':
-            return UpdateShoesSerializer
-        if self.action == 'delete_shoes':
-            return DeleteShoesSerializer
-        else:
-            return ShoesSerializer
-
-    @action(methods=['PATCH'], detail=False, url_path='update')
-    def update_shoes(self, request):
-        Serializer = self.get_serializer_class()
-
-        serializer = Serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        data = serializer.data
-
-        shoe_id = data.get('id')
-        name = data.get('name')
-        image = data.get('image')
-
-        try:
-            shoe = Shoes.objects.get(id=shoe_id)
-        except Shoes.DoesNotExist:
-            raise NotFound({'error': 'shoes with this id was not found'})
-
-        if name is not None:
-            setattr(shoe, 'name', name)
-
-        if image is not None:
-            setattr(shoe, 'image', image)
-
-        shoe.save()
-
-        shoes_dict = model_to_dict(shoe)
-        serializer = Serializer(data=shoes_dict)
-        serializer.is_valid(raise_exception=True)
-
-        return Response({'data': serializer.data})
-
-    @action(methods=['DELETE'], detail=False, url_path='delete')
-    def delete_shoes(self, request):
-        Serializer = self.get_serializer_class()
-
-        serializer = Serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        data = serializer.data
-
-        shoe_id = data.get('id')
-
-        try:
-            shoe = Shoes.objects.get(id=shoe_id)
-        except Shoes.DoesNotExist:
-            raise NotFound({'error': 'shoe with this id was not found'})
-
-        shoe.delete()
-
-        return Response({'message': f'shoe with {shoe_id} was successfully deleted'})
