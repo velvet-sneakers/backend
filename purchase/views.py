1b3cecb7e046dfe439f43a23b4ce4c11f945dc12
Mikhail Sergeev <sergeevm100@gmail.com>
2023-05-29 15:25:29 +0300

    permissions and small fixes

--- purchase/views.py
+++ purchase/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from purchase.serializers import PurchaseSerializer
 from purchase.models import Purchase
@@ -11,26 +12,24 @@ class PurchaseViewSet(ModelViewSet):
     queryset = Purchase.objects.all().order_by('-id')
     serializer_class = PurchaseSerializer
 
-    @action(methods=['POST'],detail=False, url_path="create-item")
+    @action(methods=['POST'], permission_classes=[IsAuthenticated], detail=False, url_path="create-item")
     def create_items(self, request):
-        email = request.user
+        email = request.user.email
         user = User.objects.get(email=email)
-        request.data['user'] = user.id
-        print(user)
+        request.data['user_id'] = user.id
 
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({'message':'added'})
 
-    @action(methods=['GET'], detail=False, url_path="items")
+    @action(methods=['GET'], permission_classes=[IsAuthenticated], detail=False, url_path="items")
     def get_items(self, request):
         items = Purchase.objects.all()
         data = PurchaseSerializer(items, many=True).data
-        print(data)
         return Response(data)
 
-    @action(methods=['PUT'], detail=True ,url_path="update-item")
+    @action(methods=['PUT'], permission_classes=[IsAuthenticated], detail=True, url_path="update-item")
     def update_items(self, request, pk):
         if not pk:
             return Response({'error': 'not put'})
@@ -45,7 +44,7 @@ class PurchaseViewSet(ModelViewSet):
         serializer.save()
         return Response({'message': serializer.data})
 
-    @action(methods=['DELETE'], detail=True, url_path="delete-item")
+    @action(methods=['DELETE'], permission_classes=[IsAuthenticated], detail=True, url_path="delete-item")
     def delete_items(self, request, pk):
         if not pk:
             return Response({'error': 'not put'})
