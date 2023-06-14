c9a92fbbf5287ad2c33e56febf6fda2a21045743
zavarts <zavoiskih.artyom@yandex.ru>
2023-06-14 17:58:11 +0300

    Fixed product/views.py

--- product/views.py
+++ product/views.py
@@ -1,8 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from authentication.models import User
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from product.serializers import ProductSerializer
 from product.models import Product
@@ -13,12 +12,8 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    @action(methods=['POST'], permission_classes=[IsAuthenticated], detail=False, url_path="create-item")
+    @action(methods=['POST'], permission_classes=[IsAdminUser], detail=False, url_path="create-item")
     def create_item(self, request):
-        email = request.user.email
-        user = User.objects.get(email=email)
-        request.data['user_id'] = user.id
-
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -30,18 +25,18 @@ class ProductViewSet(ModelViewSet):
     @action(methods=['GET'], permission_classes=[IsAuthenticated], detail=False, url_path="get-items")
     def get_items(self):
         items = Product.objects.all()
-        data = ProductSerializer(items, many=True).data
+        data = ProductSerializer(items).data
         return Response(data)
 
-    @action(methods=['PUT'], permission_classes=[IsAuthenticated], detail=True, url_path="update-item")
+    @action(methods=['PUT'], permission_classes=[IsAdminUser], detail=True, url_path="update-item")
     def update_item(self, request, pk):
         if not pk:
-            return Response({'error': 'not put'})
+            return Response({'error': 'must pass id'})
 
         try:
             instance = Product.objects.get(id=pk)
         except:
-            return Response({'error': 'not put'})
+            return Response({'error': 'not existing id'})
 
         serializer = self.serializer_class(data=request.data, instance=instance)
         serializer.is_valid(raise_exception=True)
@@ -51,15 +46,15 @@ class ProductViewSet(ModelViewSet):
 
         return Response({'message': serializer.data})
 
-    @action(methods=['DELETE'], permission_classes=[IsAuthenticated], detail=True, url_path="delete-item")
+    @action(methods=['DELETE'], permission_classes=[IsAdminUser], detail=True, url_path="delete-item")
     def delete_item(self, request, pk):
         if not pk:
-            return Response({'error': 'not put'})
+            return Response({'error': 'must pass id'})
 
         try:
             item = Product.objects.get(id=pk)
         except:
-            return Response({'error': 'not put'})
+            return Response({'error': 'not existing id'})
 
         send_email.delay('Удалён продукт', f'Удалён продукт с id {item.id}')
 
