507e56b164ddd0dbfc04ce5ca5599c7dc8c16464
AndrewwKovv <kovshov.K@yandex.ru>
2023-06-19 21:15:05 +0300

    fix task

--- shop/tasks.py
+++ shop/tasks.py
@@ -1,6 +1,4 @@
 from celery import shared_task
-from celery.task import periodic_task
-from datetime import timedelta
 from django.core.mail import send_mail
 
 
@@ -25,7 +23,7 @@ def send_email_created_orders(id):
         fail_silently=True
     )
 
-@periodic_task(run_every=timedelta(minutes=2880))
+@shared_task
 def send_email_updated_orders(id):
     send_mail(
         'Заказ обновлен',
