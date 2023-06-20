416fc7657b1346ba9ef51e0073c0ef0ffb5a0d86
Emil <95440091+Emil-Ka@users.noreply.github.com>
2023-06-21 00:31:59 +0300

    added cache for shop views (#31)
    Co-authored-by: emilkaraev <EKaraev@alfabank.ru>

--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 
 from authentication.models import User
 from shop.models import Shoes, ShopItems, Order
@@ -10,6 +11,9 @@ from shop.serializers import ShoesSerializer, ShopItemsSerializer, OrderSerializ
 from .tasks import send_email_created_shoes, send_email_updated_shoes, send_email_created_orders, \
     send_email_updated_orders, send_email_destroyed_shoes, send_email_created_item, send_email_updated_item, \
     send_email_deleted_item
+
+CACHE_SHOES_KEY = 'shoes'
+CACHE_TIMEOUT = 3600
 
 
 class ShoesViewSet(ModelViewSet):
@@ -22,12 +26,28 @@ class ShoesViewSet(ModelViewSet):
         else:
             return [IsAdminUser()]
 
+    @staticmethod
+    def set_cache():
+        data = ShoesSerializer(Shoes.objects.all(), many=True).data
+        cache.set(CACHE_SHOES_KEY, data, timeout=CACHE_TIMEOUT)
+
+        return data
+
+    @action(methods=['GET'], detail=False)
+    def get(self, request, *args, **kwargs):
+        data = cache.get(CACHE_SHOES_KEY)
+
+        if not data:
+            data = self.set_cache()
+
+        return Response(data)
+
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
 
         send_email_created_shoes.delay(response.data.get("name"))
 
-        cache.set('shoes', response.data, timeout=3600)
+        self.set_cache()
 
         return response
 
@@ -36,12 +56,16 @@ class ShoesViewSet(ModelViewSet):
 
         send_email_updated_shoes.delay(response.data.get("name"))
 
+        self.set_cache()
+
         return response
 
     def destroy(self, request, *args, **kwargs):
         response = super().destroy(request, *args, **kwargs)
 
         send_email_destroyed_shoes.delay(response.data.get("id"))
+
+        self.set_cache()
 
         return response
 
@@ -83,7 +107,7 @@ class ShopItemsViewSet(ModelViewSet):
     queryset = ShopItems.objects.all().order_by('-id')
     serializer_class = ShopItemsSerializer
 
-    @action(methods=['POST'],detail= False, url_path="create-items")
+    @action(methods=['POST'], detail=False, url_path="create-items")
     def create_items(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -93,22 +117,19 @@ class ShopItemsViewSet(ModelViewSet):
 
         items = ShopItems.objects.all()
         data = ShopItemsSerializer(items, many=True).data
-        cache.set('shop_items', data, timeout=3600)
+        cache.set('shop_items', data, timeout=CACHE_TIMEOUT)
 
         return Response({'message': 'added'})
 
     @action(methods=['GET'], detail=False, url_path="items")
     def get_items(self, request):
-        # Пытаемся получить данные из кеша
         data = cache.get('shop_items')
+
         if not data:
-            print("Fetching data from the database")
-            # Если данных в кеше нет, то получаем их из базы данных и добавляем в кеш
             items = ShopItems.objects.all()
             data = ShopItemsSerializer(items, many=True).data
-            cache.set('shop_items', data, timeout=3600)
+            cache.set('shop_items', data, timeout=CACHE_TIMEOUT)
         else:
-            #Когда сделаете удалите принты для проверки
             print("Fetching data from cache")
 
         return Response(data)
@@ -131,7 +152,7 @@ class ShopItemsViewSet(ModelViewSet):
 
         items = ShopItems.objects.all()
         data = ShopItemsSerializer(items, many=True).data
-        cache.set('shop_items', data, timeout=3600)
+        cache.set('shop_items', data, timeout=CACHE_TIMEOUT)
 
         return Response({'data': data})
 
@@ -151,7 +172,7 @@ class ShopItemsViewSet(ModelViewSet):
 
         items = ShopItems.objects.all()
         data = ShopItemsSerializer(items, many=True).data
-        cache.set('shop_items', data, timeout=3600)
+        cache.set('shop_items', data, timeout=CACHE_TIMEOUT)
 
         return Response({'message': 'items delete'})
 
